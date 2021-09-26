b21f41cc34cc79d7846b72b804745b5a92d02241
Oleksii Grygorchuk <o.grygorchuk@snapaddy.com>
2021-09-26 10:43:17 +0200

    add(): table resizing logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,56 +1,72 @@
 const CODES = {
-    A: 65,
-    Z: 90
+  A: 65,
+  Z: 90
+};
+
+function toCell(_, index) {
+  return `
+    <div class="cell" contenteditable data-col="${index}"></div>
+    `;
 }
 
-function toCell() {
-    return `
-    <div class="cell" contenteditable></div>
-    `
-}
-
-function toColumn(col) {
-    return `
-    <div class="column">${col}</div>
-    `
+function toColumn(col, index) {
+  return `
+    <div class="column" data-type="resizable" data-col="${index}">
+        ${col}
+        <div class="col-resize" data-resize="col"></div>
+    </div>
+    `;
 }
 
 function createRow(index, content) {
-    return `
-    <div class="row">
-        <div class="row-info">${index ? index : ''}</div>
+  // eslint-disable-next-line max-len
+  const resizer = index ? `<div class="row-resize" data-resize="row"></div>` : '';
+  return `
+    <div class="row" data-type="resizable">
+        <div class="row-info">
+            ${index ? index : ''}
+            ${resizer}
+        </div>
         <div class="row-data">${content}</div>
     </div>
-    `
+    `;
 }
 
 function toChar(_, index) {
-    return String.fromCharCode(CODES.A + index)
+  return String.fromCharCode(CODES.A + index);
 }
 
+/**
+ * Creates excel table
+ * @param rowsCount amount of rows in the table
+ * @returns
+ */
 export function createTable(rowsCount = 15) {
-    const colsCount = CODES.Z - CODES.A + 1
-    const rows = []
+  const colsCount = CODES.Z - CODES.A + 1;
+  const rows = [];
 
-    const cols = new Array(colsCount)
-        .fill('')
-        .map(toChar)
-        .map(toColumn)
-        .join('')
+  // create table cols
+  const cols = new Array(colsCount)
+  // fill array with empty string
+  .fill('')
+  // fill each element of array with alphabet literal
+  .map(toChar)
+  // make column out of each array element
+  .map(toColumn)
+  // convert array into string with empty space as divider
+  .join('');
 
-    rows.push(createRow(null, cols))
+  // create rows with createdCols as basis
+  rows.push(createRow(null, cols));
 
-    /* const cells = new Array(colsCount)
+  /* const cells = new Array(colsCount)
         .fill('')
         .map(toCell)
         .join('')*/
 
-    for (let i = 0; i < rowsCount; i++) {
-        const cells = new Array(colsCount)
-            .fill('')
-            .map(toCell)
-            .join('')
-        rows.push(createRow(i+1, cells))
-    }
-    return rows.join('')
+  for (let i = 0; i < rowsCount; i++) {
+    const cells = new Array(colsCount).fill('').map(toCell).join('');
+    rows.push(createRow(i + 1, cells));
+  }
+  return rows.join('');
 }
