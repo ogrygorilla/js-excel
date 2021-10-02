bfc9a1110bc7eaaf0d2638a8f1f702ca8693f86c
Oleksii Grygorchuk <o.grygorchuk@snapaddy.com>
2021-10-02 10:49:00 +0200

    add(): fomula-table input bindig

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,28 +1,53 @@
-import {ExcelComponent} from '@core/ExcelComponent'
+import { ExcelComponent } from '@core/ExcelComponent';
+import { $ } from '@core/dom';
 
 export class Formula extends ExcelComponent {
-    static className = 'excel__formula'
+  static className = 'excel__formula';
 
-    constructor($root) {
-        super($root, {
-            name: 'Formula',
-            listeners: ['input', 'click']
-        })
-    }
+  constructor($root, options) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click', 'keydown'],
+      ...options
+    });
+    this.emitter = options.emitter;
+  }
 
-    toHTML() {
-        return `
+  init() {
+    super.init();
+    this.$formula = this.$root.find('#formula');
+    this.$on('table:cellSelection', ($cell) => {
+      this.$formula.text($cell.text());
+    });
+
+    this.$on('table:input', (text) => {
+      this.$formula.text(text);
+    });
+  }
+
+  toHTML() {
+    return `
         <div class="info">fx</div>
-            <div class="input" contenteditable spellcheck="false"></div>
-        `
-    }
+            <div id="formula" class="input" contenteditable spellcheck="false"></div>
+        `;
+  }
 
-    onInput(event) {
-        console.log(this.$root)
-        console.log('Formula: onInput', event.target.textContent.trim())
-    }
+  onInput(event) {
+    // console.log(this.$root);
+    console.log($(event.target).text());
+    this.$emit('formula:input', $(event.target).text());
+  }
 
-    onClick() {
-        console.log('click');
+  onClick() {
+    console.log('click');
+  }
+
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab'];
+    const { key } = event;
+    if (keys.includes(key)) {
+      event.preventDefault();
+      this.$emit('formula:enter');
     }
+  }
 }
