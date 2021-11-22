940c99cc8b7125a8e1b7e4e4c2362708a16a8e00
Oleksii Grygorchuk <o.grygorchuk@snapaddy.com>
2021-11-22 17:04:42 +0100

    fix() heroku deployment

--- heroku.js
+++ heroku.js
@@ -3,14 +3,13 @@ const path = require('path');
 const port = process.env.PORT || 3333;
 
 const server = http.createServer((req, res) => {
-  if (req.url !== '/favicon.ico') {
-    path.resolve(__dirname, './dist/favicon.ico');
+  if (req.url === '/favicon.ico') {
+    res.sendFile(path.resolve(__dirname, './dist/favicon.ico'));
   } else {
-    path.resolve(__dirname, './dist/index.html');
-    res.end();
+    res.sendFile(path.resolve(__dirname, './dist/index.html'));
   }
 });
 
 server.listen(port, () => {
-  console.log('ExcelJs is runing on the heroku serer');
+  console.log('ExcelJs is runing on the heroku server');
 });
