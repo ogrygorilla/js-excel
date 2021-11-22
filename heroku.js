aa14b51ab613c853089e04752dc3dbf30e86eaa5
Oleksii Grygorchuk <o.grygorchuk@snapaddy.com>
2021-11-22 17:38:33 +0100

    fix(): h d

--- heroku.js
+++ heroku.js
@@ -6,10 +6,14 @@ const port = process.env.PORT || 3333;
 const server = http.createServer((req, res) => {
   if (req.url === '/favicon.ico') {
     const icon = fs.readFileSync(path.resolve(__dirname, './dist/favicon.ico'), 'utf8');
+    console.log(icon);
+    res.setHeader('Content-Type', 'application/json');
     res.write(icon);
     res.end();
   } else {
     const indexHtml = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf8');
+    console.log(indexHtml);
+    res.setHeader('Content-Type', 'application/json');
     res.write(indexHtml);
     res.end();
   }
